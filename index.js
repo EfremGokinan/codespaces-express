bbaaf650d86b25f2c25181e55e62beffde7e5c97
EfremGokinan <55692736+EfremGokinan@users.noreply.github.com>
2023-04-22 15:16:42 +0000

    start

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-let app = require('express')();
-let server = require('http').createServer(app);
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 const path = require('path');
 
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
-});
 const cors = require('cors');
 app.use(cors());
 var port = process.env.PORT || 3000;
