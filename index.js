156d449bc0e3b0948912e2675eca508379c3f1bc
EfremGokinan <55692736+EfremGokinan@users.noreply.github.com>
2023-04-22 15:02:17 +0000

    start

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-const express = require('express')
-const app = express()
-const port = 3000
+let app = require('express')();
+let server = require('http').createServer(app);
+const path = require('path');
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+const cors = require('cors');
+app.use(cors());
+var port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname+'/index.html'));
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+io.on('connection', (socket) => {
+  
+  console.log('a user connected');
+  
+  socket.on('order-changed', (text) => {
+    console.log(socket.rooms);
+    console.log('message from client: ', text);
+    io.emit('update-changes', text);
+  });
+
+  socket.on('message', (text) => {
+    // socket.join(text.room_id)
+    console.log(socket.rooms)
+    console.log('message-send', text);
+    io.emit('message-send', text);
+  });
+
+  socket.on("disconnect", () => console.log("Client disconnected"));
+
+});
+
+server.listen(port, function(){
+   console.log('listening on *: ' + port);
+});
